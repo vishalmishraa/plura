7940151334f95121493bac824d0e53ae437e7310
vishal mishra <vishal.mishra9470@gmail.com>
2024-02-01 22:48:36 +0530

    Add agency and domain pages

---
+++ src/app/(main)/agency/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AgencyPage = () => {
+  return (
+    <div>
+      Agency page
+    </div>
+  )
+}
+
+export default AgencyPage
---
+++ src/app/[domain]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DomainPage = () => {
+  return (
+    <div>
+      Coustom domain page
+    </div>
+  )
+}
+
+export default DomainPage
