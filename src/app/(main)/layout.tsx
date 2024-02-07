5168c40f45b48f93bc32fe1b35f053050aebf53f
vishal mishra <vishal.mishra9470@gmail.com>
2024-02-07 12:37:13 +0530

    Refactor layout component

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,9 +7,7 @@ import {dark} from "@clerk/themes";
 const Layout = ({children}:{children:React.ReactNode}) => {
   return (
     <ClerkProvider appearance={{baseTheme:dark}}>
-                    
                          {children}
-                    
     </ClerkProvider>
 
    
