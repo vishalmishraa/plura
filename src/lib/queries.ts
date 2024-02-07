5c3ff08f969cadcfc6579f0b5ec1e8cd1d9201f8
vishal mishra <vishal.mishra9470@gmail.com>
2024-02-07 15:20:26 +0530

    implement initUser function

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -4,6 +4,7 @@ import { clerkClient, currentUser } from "@clerk/nextjs"
 import { db } from "./db"
 import { redirect } from "next/navigation"
 import { Agency, User } from "@prisma/client"
+import { createClerkClient } from "@clerk/nextjs/server"
 
 export const getAuthUserDetails = async ()=>{
     const user = await currentUser()
@@ -190,8 +191,29 @@ export const deleteAgency = async (agencyId : string)=>{
   const response = await db.agency.delete({
     where : {id : agencyId}
   })
-
-
-  
   return response;
+}
+
+export const initUser = async (newUser: Partial<User>)=>{
+    const user = await currentUser();
+    if(!user) return;  
+
+    const userData = await db.user.upsert({
+        where : {email : user.emailAddresses[0].emailAddress},
+        update : {...newUser},
+        create : {
+          id: user.id,
+          avatarUrl : user.imageUrl,
+          email : user.emailAddresses[0].emailAddress,
+          name : `${user.firstName} ${user.lastName}`,  
+          role : newUser.role || 'SUBACCOUNT_USER'
+        }
+    })
+
+    await clerkClient.users.updateUserMetadata(user.id, {
+        privateMetadata: {
+          role: userData.role || 'SUBACCOUNT_USER',
+        },
+      })
+      return userData;
 }
